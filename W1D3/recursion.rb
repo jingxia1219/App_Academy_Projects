bf295f74c94f23d5f89529ae4b573b65e17a8bf4
jingxia1219 <jingxia1219@gmail.com>
2018-06-25 23:47:13 -0700

    add W2D1 Mancara hw with 4 failed specs in make_move

--- W1D3/recursion.rb
+++ W1D3/recursion.rb
@@ -1,11 +1,10 @@
+require 'byebug'
 def pascal(num)
   return [1] if num.zero?
   result = [1]
   prev = pascal(num - 1)
 
   (0..prev.length - 2).each do |i|
-    p i
-    p prev
     result << (prev[i] + prev[i + 1])
   end
 
@@ -16,6 +15,25 @@ def pascal(num)
  # p pascal(2)
  # p pascal(3)
  # p pascal(4)
+
+
+
+ def rec_flatten(arr)
+  result = []
+  # debugger
+  return arr if arr.empty?
+   arr.each do |el|
+     if el.is_a?(Array)
+       result += rec_flatten(el)
+     else
+       result << el
+     end
+   end
+   result
+ end
+
+ rec_flatten([[1],2,[3]])
+
 
 
 def range(min, max)
@@ -78,15 +96,7 @@ end
 class Array
 
   def deep_dup
-    dupped_arr =[]
-    self.each do |el|
-      if el.is_a?(Array)
-        dupped_arr << el.deep_dup
-       else
-         dupped_arr << el.dup
-      end
-    end
-    dupped_arr
+    self.map { |el| el.is_a?(Array) ? el.deep_dup : el }
   end
 
 end
@@ -96,7 +106,7 @@ robot_parts = [
   ["capacitors", "resistors", "inductors"]
 ]
 robot_parts_copy = robot_parts.dup
-# robot_parts.deep_dup
+robot_parts.deep_dup
 robot_parts_copy << "mutate?"
 # p robot_parts
 
@@ -122,22 +132,25 @@ end
 # p fibonacci_rec(4)
 # p fibonacci_rec(5)
 # p fibonacci_rec(8)
+=begin
+The binary search algorithm begins by comparing the target value to the value of the middle element of the sorted array. If the target value is equal to the middle element's value, then the position is returned and the search is finished. If the target value is less than the middle element's value, then the search continues on the lower half of the array; or if the target value is greater than the middle element's value, then the search continues on the upper half of the array. This process continues, eliminating half of the elements, and comparing the target value to the value of the middle element of the remaining elements - until the target value is either found (and its associated element position is returned), or until the entire array has been searched (and "not found" is returned).
 
+Write a recursive binary search: bsearch(array, target). Note that binary search only works on sorted arrays. Make sure to return the location of the found object (or nil if not found!). Hint: you will probably want to use subarrays.
 
+Make sure that these test cases are working:
+=end
 def bsearch(array, target)
-  return nil if array.empty?
-  mid_point = array.length/2
-
-  return mid_point  if target == array[mid_point]
-  if array[mid_point] < target
-    if bsearch(array[(mid_point+1)..-1],target) == nil
-      return nil
+  return nil  if array.empty?
+  mid_idx = array.length / 2
+   # debugger
+    if array[mid_idx] == target
+      return mid_idx
+    elsif array[mid_idx] > target
+      bsearch(array[0...mid_idx], target)
     else
-      mid_point + 1 + bsearch(array[(mid_point+1)..-1],target)
+      return nil unless bsearch(array[mid_idx+1..-1], target)
+       mid_idx + 1 + bsearch(array[mid_idx+1..-1], target)
     end
-  else
-    return bsearch(array[0...mid_point],target) if array[mid_point] > target
-  end
 end
 
 
@@ -159,17 +172,15 @@ def merge
 end
 
 def subsets(arr)
-  return [arr] if arr.empty?
-
-  popped_el = arr[-1]
-  temp_arr2 = subsets(arr[0..-2])
-  temp_arr = temp_arr2.map { |sub_array| sub_array << popped_el }
-  temp_arr + temp_arr2
+  result = [[]]
+  last_el = arr[-1]
+  return [[]] if last_el.nil?
+  result.map { |el| el + [last_el] }
 
 end
 
 p subsets([]) # => [[]]
 p subsets([1]) # => [[], [1]]
-p subsets([1, 2]) # => [[], [1], [2], [1, 2]]
-p subsets([1, 2, 3])
+# p subsets([1, 2]) # => [[], [1], [2], [1, 2]]
+# p subsets([1, 2, 3])
 # => [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
