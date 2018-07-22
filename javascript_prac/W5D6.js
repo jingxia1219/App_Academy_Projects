f7f4206650105177f21fbf0245674b2934888f4c
Jing <jingxia1219@gmail.com>
2018-07-21 22:55:46 -0700

    add webpack-demo

---
+++ javascript_prac/W5D6.js
@@ -0,0 +1,11 @@
+// const hasArgs = function() {
+//   debugger
+//   const noArgs = () => arguments[0];
+//   return noArgs('FakeArg');
+// };
+// console.log(hasArgs('RealArg'));
+// console.log('hi');
+var double = num => num * 2
+const noArgs = args => args;
+console.log(noArgs('no arg?'))
+console.log(double(3))
